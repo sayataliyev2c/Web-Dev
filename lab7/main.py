b63016434ba64d08f2a09fb81d7ca7bcec6ebe77
sayataliyev2c <159242175+sayataliyev2c@users.noreply.github.com>
2026-03-17 15:26:12 +0500

    Update main.py

--- lab7/main.py
+++ lab7/main.py
@@ -1,1 +1,16 @@
+from models import Animal, Dog, Cat
 
+
+dog = Dog("Buddy", 3, "Brown", "Labrador")
+cat = Cat("Misty", 2, "White", 9)
+animal = Animal("Generic", 5, "Gray")
+
+
+animals = [dog, cat, animal]
+
+
+for a in animals:
+    print(a)
+    print(a.speak())
+    print(a.info())
+    print()
